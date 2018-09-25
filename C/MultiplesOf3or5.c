0aca88d64ba423157b33d75a933eabdca31b6a44
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2018-09-25 10:44:51 -0300

    Folder rearrangement
    Creating separate folders for different programming languages. At this point, previous Python puzzles where included in the Python folder and a C folder was created

---
+++ C/MultiplesOf3or5.c
@@ -0,0 +1,16 @@
+/*  Multiples of 3 or 5   -   CodeWars
+    Nilton Gomes Martins Junior
+    24/09/2018
+    https://www.codewars.com/kata/514b92a657cdc65150000006/train/c
+*/
+
+int solution(int number) {
+    if (number == 3) {
+
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
