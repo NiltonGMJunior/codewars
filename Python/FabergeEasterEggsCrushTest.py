15641c977e21ca74112d8e446b188709bb455868
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2019-01-21 21:43:38 -0200

    Update

---
+++ Python/FabergeEasterEggsCrushTest.py
@@ -0,0 +1,11 @@
+#   Fabergè Easter Eggs crush test - CodeWars
+#   Nilton Gomes Martins Junior
+#   19/01/2019
+#   https://www.codewars.com/kata/54cb771c9b30e8b5250011d4/train/python
+
+def height(n, m):
+    # TODO: Implement this function
+
+if __name__ == "__main__":
+    n, m = list(map(int, input().split()))
+    print(height(n, m))
---
+++ Python/LastDigitOfN^2==N.py
@@ -0,0 +1,15 @@
+#   Last digits of N^2 == N - CodeWars
+#   Nilton Gomes Martins Junior
+#   19/01/2019
+#   https://www.codewars.com/kata/last-digits-of-n-2-equals-equals-n/train/python
+
+def green(n):
+    '''
+    green(n) returns the n-th number such that the last digits of n**2 is n
+    '''
+
+    
+
+if __name__ == "__main__":
+    n = int(input())
+    print(green(n))
